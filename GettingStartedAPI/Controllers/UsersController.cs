07d627fe8f18c01ce943e304e401fb81fb94d9aa
Guilherme Reis <guitrading@outlook.com>
2023-05-28 20:57:53 -0400

    First commit after cleanup

---
+++ GettingStartedAPI/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GettingStartedAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    // GET: api/<UsersController> == api/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<UsersController>/5 = = api/Users/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST creates a new record
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // PATCH updates PART of a record
+    [HttpPatch(template: "{id}")]
+    public void Patch(int id, [FromBody] string emailAddress)
+    {
+    }
+
+
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
